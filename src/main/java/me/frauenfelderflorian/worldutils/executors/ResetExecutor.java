bd0056d420fd02b0e72acc322d11609ee1572f7a
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-08-08 12:47:28 +0200

    add basic reset functionality

--- src/main/java/me/frauenfelderflorian/worldutils/executors/ResetExecutor.java
+++ src/main/java/me/frauenfelderflorian/worldutils/executors/ResetExecutor.java
@@ -1,13 +1,41 @@
 package me.frauenfelderflorian.worldutils.executors;
 
+import me.frauenfelderflorian.worldutils.WorldUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public record ResetExecutor(JavaPlugin plugin) implements CommandExecutor {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+public record ResetExecutor(WorldUtils plugin) implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1 && args[0].equals("confirm")) {
+            //another possibility: onLoad deletion of worlds: settings
+            String[] worlds = new String[]{"world", "world_nether", "world_the_end"};
+            for (String world : worlds) {
+                Bukkit.getServer().unloadWorld(world, true);
+                deleteFolder(world);
+            }
+            //set seed?
+            //Bukkit.spigot().restart();
+            return true;
+        }
         return false;
+    }
+
+    private void deleteFolder(String folderName) {
+        Path folder = Paths.get(folderName);
+        try {
+            Files.walk(folder).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
