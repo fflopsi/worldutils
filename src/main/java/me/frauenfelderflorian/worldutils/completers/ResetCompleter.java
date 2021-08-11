4a734155730aa56f50f16feccd670529f9cabf8b
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-08-11 07:57:06 +0200

    fix tab completion

--- src/main/java/me/frauenfelderflorian/worldutils/completers/ResetCompleter.java
+++ src/main/java/me/frauenfelderflorian/worldutils/completers/ResetCompleter.java
@@ -3,6 +3,7 @@ package me.frauenfelderflorian.worldutils.completers;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,7 @@ public class ResetCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> completions = new ArrayList<>();
-        if (args.length == 1) completions.add("confirm");
+        if (args.length == 1) StringUtil.copyPartialMatches(args[0], List.of("confirm"), completions);
         return completions;
     }
 }
