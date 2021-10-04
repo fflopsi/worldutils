7137423ecbd24399d9167955b655f79369ea49c1
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-04 19:16:12 +0200

    remove tab completion of argument "confirm"

--- src/main/java/me/frauenfelderflorian/worldutils/commands/ResetCommand.java
+++ src/main/java/me/frauenfelderflorian/worldutils/commands/ResetCommand.java
@@ -8,9 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.StringUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -59,9 +57,6 @@ public record ResetCommand(JavaPlugin plugin) implements TabExecutor {
      */
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        List<String> completions = new ArrayList<>();
-        if ((Boolean) WorldUtils.prefs.get(Options.RESET_NEED_CONFIRM) && args.length == 1)
-            StringUtil.copyPartialMatches(args[0], List.of("confirm"), completions);
-        return completions;
+        return List.of();
     }
 }
