57111535329e2b3b2240c308a00fe621995c934c
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-11 15:37:49 +0200

    fix tab completion

--- src/main/java/me/frauenfelderflorian/worldutils/commands/PersonalPositionCommand.java
+++ src/main/java/me/frauenfelderflorian/worldutils/commands/PersonalPositionCommand.java
@@ -123,12 +123,14 @@ public record PersonalPositionCommand(JavaPlugin plugin) implements TabExecutor 
         switch (args.length) {
             case 1 -> {
                 //command or position name being entered
-                StringUtil.copyPartialMatches(args[0], List.of("list", "clear", "tp", "del"), completions);
+                StringUtil.copyPartialMatches(args[0], List.of("list", "clear", "del"), completions);
+                if (sender instanceof Player && sender.isOp())
+                    StringUtil.copyPartialMatches(args[0], List.of("tp"), completions);
                 StringUtil.copyPartialMatches(args[0], positions.getPositions(), completions);
             }
             case 2 -> {
                 //position name being entered
-                if (List.of("tp", "del").contains(args[0]))
+                if (args[0].equals("del") || sender instanceof Player && sender.isOp() && args[0].equals("tp"))
                     StringUtil.copyPartialMatches(args[1], positions.getPositions(), completions);
             }
         }
