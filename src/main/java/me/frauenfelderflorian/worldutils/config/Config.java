22565b953cd783d08e827392132b002b33137e74
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-17 14:16:32 +0200

    remove unused import

--- src/main/java/me/frauenfelderflorian/worldutils/config/Config.java
+++ src/main/java/me/frauenfelderflorian/worldutils/config/Config.java
@@ -2,7 +2,6 @@ package me.frauenfelderflorian.worldutils.config;
 
 import me.frauenfelderflorian.worldutils.WorldUtils;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
