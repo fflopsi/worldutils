e56c311b441cc09844c0d382fd0bbee21d6df3e7
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-11-05 07:19:25 +0100

    add methods for personal positions smaller adjustments

--- src/main/java/me/frauenfelderflorian/worldutils/config/Positions.java
+++ src/main/java/me/frauenfelderflorian/worldutils/config/Positions.java
@@ -2,6 +2,8 @@ package me.frauenfelderflorian.worldutils.config;
 
 import me.frauenfelderflorian.worldutils.WorldUtils;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 
 import java.util.Set;
 
@@ -20,54 +22,96 @@ public class Positions extends Config {
     }
 
     /**
+     * Set a personal position
+     *
+     * @param player the owner of the position
+     * @param name   the name of the position
+     */
+    public void setPersonal(Player player, String name) {
+        set(player.getUniqueId() + "." + name, player.getLocation(), true);
+    }
+
+    /**
      * Set the author of a position
      *
-     * @param position the position name
-     * @param author   the author's name
+     * @param name   the position name
+     * @param author the author's name
      */
-    public void setAuthor(String position, String author) {
-        set("list." + position, author, true);
+    public void setAuthor(String name, String author) {
+        set("list." + name, author, true);
+    }
+
+    /**
+     * Check if the personal position exists
+     *
+     * @param player the Player whose personal positions to check
+     * @param name   the name of the position to check
+     * @return true if something is found, false otherwise
+     */
+    public boolean containsPersonal(Player player, String name) {
+        return config.contains(player.getUniqueId() + "." + name);
     }
 
     /**
      * Check if the position has a saved author
      *
-     * @param position the position to be checked
+     * @param name the position to be checked
      * @return true if an author was found, false otherwise
      */
-    public boolean containsAuthor(String position) {
-        return config.contains("list." + position);
+    public boolean containsAuthor(String name) {
+        return config.contains("list." + name);
     }
 
     /**
      * Get a String from a path
      *
-     * @param position the position
+     * @param name the position name
      * @return String of the author of the position
      */
-    public String getAuthor(String position) {
-        return config.getString("list." + position);
+    public String getAuthor(String name) {
+        return config.getString("list." + name);
     }
 
     /**
      * Get a Location from a path
      *
-     * @param path the path of the Location
+     * @param name the name of the Location
      * @return the Location at the path
      */
-    public Location getLocation(String path) {
-        return config.getLocation(path);
+    public Location getLocation(String name) {
+        return config.getLocation(name);
+    }
+
+    /**
+     * Get a personal Location from a Player and the position name
+     *
+     * @param player the Player
+     * @param name   the name of the position
+     * @return the Location at the path
+     */
+    public Location getPersonalLocation(Player player, String name) {
+        return config.getLocation(player.getUniqueId() + "." + name);
     }
 
     /**
      * Remove a position
      *
-     * @param position the position to be removed
+     * @param name the name of the position
      */
     @Override
-    public void remove(String position) {
-        super.remove(position);
-        if (containsAuthor(position)) super.remove("list." + position);
+    public void remove(String name) {
+        super.remove(name);
+        if (containsAuthor(name)) super.remove("list." + name);
+    }
+
+    /**
+     * Remove a (personal) position
+     *
+     * @param player the owner of the position
+     * @param name   the name of the position
+     */
+    public void remove(Player player, String name) {
+        super.remove(player.getUniqueId() + "." + name);
     }
 
     /**
@@ -79,5 +123,19 @@ public class Positions extends Config {
         Set<String> keys = config.getKeys(false);
         keys.remove("list");
         return keys;
+    }
+
+    /**
+     * Get the personal positions of a Player
+     *
+     * @param player the Player
+     * @return a Set of Strings containing all position names
+     */
+    public Set<String> getPositions(Player player) {
+        ConfigurationSection positions = config.getConfigurationSection(player.getUniqueId().toString());
+        if (positions != null) {
+            return positions.getKeys(false);
+        }
+        return null;
     }
 }
