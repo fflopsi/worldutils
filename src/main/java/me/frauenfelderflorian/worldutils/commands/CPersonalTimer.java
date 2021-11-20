bc48d9e718ba574b6a5d4b4ec6bc4c5c53082244
flopsi <florian.l.frauenfelder@gmail.com>
2021-11-20 15:23:32 +0100

    add CPersonalTimer.java

---
+++ src/main/java/me/frauenfelderflorian/worldutils/commands/CPersonalTimer.java
@@ -0,0 +1,40 @@
+package me.frauenfelderflorian.worldutils.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.List;
+
+/**
+ * CommandExecutor and TabCompleter for command personaltimer
+ */
+public class CPersonalTimer implements TabExecutor {
+    /**
+     * Done when command sent
+     *
+     * @param sender  sender of the command
+     * @param command sent command
+     * @param alias   used alias
+     * @param args    used arguments
+     * @return true if correct command syntax used and no errors, false otherwise
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        return false;
+    }
+
+    /**
+     * Done while entering command
+     *
+     * @param sender  sender of the command
+     * @param command sent command
+     * @param alias   used alias
+     * @param args    used arguments
+     * @return List of Strings for tab completion
+     */
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}
